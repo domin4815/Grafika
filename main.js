2bb5fa24da42af4dc26c081530e44a3cc5511393
Michał Pałka <prolativus@gmail.com>
2015-06-09 11:03:40 +0200

    Initial scheme

---
+++ main.js
@@ -0,0 +1,57 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+var webGLRenderer = new THREE.WebGLRenderer();
+webGLRenderer.setClearColor(new THREE.Color(0xEEEEEE, 1.0));
+webGLRenderer.setSize(window.innerWidth, window.innerHeight);
+webGLRenderer.shadowMapEnabled = true;
+$("#WebGL-output").append(webGLRenderer.domElement);
+
+var ambiLight = new THREE.AmbientLight(0x141414);
+scene.add(ambiLight);
+
+var light = new THREE.DirectionalLight();
+light.position.set(0, 30, 20);
+scene.add(light);
+
+var planets = [];
+
+function init(){
+	camera.position.x = 30;
+	camera.position.y = 12;
+	camera.position.z = 28;
+	camera.lookAt(new THREE.Vector3(0, 0, 0));
+	
+	var planet1 = createPlanet(6, 0.5, "floor-wood.jpg", 0.02);
+	planet1.position.x = 0;
+	planets.push(planet1);
+	
+	var planet2 = createPlanet(4, 0.5, "metal-rust.jpg", 0.04);
+	planet2.setGravitySource(planet1, 20, 0, 0.01);
+	planets.push(planet2);
+	
+	var planet3 = createPlanet(2, 0.5, "floor-wood.jpg", 0.03);
+	planet3.setGravitySource(planet2, 10, 0, 0.03);
+	planets.push(planet3);
+	
+	var planet4 = createPlanet(4, 0.5, "metal-rust.jpg", 0.04);
+	planet4.setGravitySource(planet1, 20, Math.PI/2, 0.01);
+	planets.push(planet4);
+	
+	for(var i=0; i<planets.length; ++i){
+		scene.add(planets[i]);
+	}
+}
+
+function render() {  
+    for(var i=0; i<planets.length; ++i){
+		planets[i].move();
+	}
+    
+    requestAnimationFrame(render);
+    webGLRenderer.render(scene, camera);
+}
+
+init();
+
+render();
---
+++ planet.js
@@ -0,0 +1,33 @@
+function createMesh(geom, imageFile) {
+    var texture = THREE.ImageUtils.loadTexture("textures/" + imageFile);
+    var mat = new THREE.MeshPhongMaterial();
+    mat.map = texture;
+
+    var mesh = new THREE.Mesh(geom, mat);
+    return mesh;
+}
+
+function createPlanet(radius, density, textureName, rotationSpeed){
+	var planet = createMesh(new THREE.SphereGeometry(radius, 20, 20), textureName);
+	planet.radius = radius;
+	planet.mass = radius*radius*radius*3.14*4/3;
+	planet.rotationSpeed = rotationSpeed;
+	planet.gravitySource = null;
+	
+	planet.move = function(){
+		planet.rotation.y += planet.rotationSpeed;
+		if(planet.gravitySource != null){
+			planet.orbitPhase += planet.orbitSpeed;
+			planet.position.x = planet.gravitySource.position.x + planet.gravitySourceDistance * Math.cos(planet.orbitPhase);
+			planet.position.z = planet.gravitySource.position.z + planet.gravitySourceDistance * Math.sin(planet.orbitPhase);
+		}
+	};
+	
+	planet.setGravitySource = function(source, distance, orbitPhase, orbitSpeed){
+		planet.gravitySource = source;
+		planet.gravitySourceDistance = distance;
+		planet.orbitPhase = orbitPhase;
+		planet.orbitSpeed = orbitSpeed;
+	};
+	return planet;
+}
